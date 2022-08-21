bbe4294488664409e23807cd8148d576951b7f5e
Brian <abrian130@gmail.com>
2022-08-20 12:16:55 -1200

    Todo list project complete

---
+++ src/components/TodoInput.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export default class TodoInput extends Component {
+  render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props
+    return (
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+            <div className="input-group">
+                <div className="input-group-prepend">
+                    <div className="input-group-text bg-primary text-white">
+                        <i className="fas fa-book"></i>
+                    </div>
+                </div>
+                <input 
+                    type="text" 
+                    className="form-control text-capitalize" 
+                    placeholder="add a todo item"
+                    value={item} onChange={handleChange}
+                />
+            </div>
+            <button type="submit" 
+                className={
+                    editItem 
+                    ? "btn btn-success btn-block mt-3" 
+                    : "btn btn-primary btn-block mt-3"
+                }
+            >
+                {editItem ? "edit item" : "add item"} 
+            </button>
+        </form>
+      </div>
+    )
+  }
+}
